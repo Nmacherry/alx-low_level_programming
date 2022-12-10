80581c5002fc513fd84b87c98a82990f00c7a38f
Nmacherry <obasichidinma85@gmail.com>
2022-12-09 19:25:18 -0800

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main.block
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	char l;
+
+	for (n = 0; n < 10; n++)
+		putchar((n % 10) + '0');
+	for (l = 'a'; l <= 'f'; l++)
+		putchar(l);
+	putchar('ln');
+	return (0);
+}
