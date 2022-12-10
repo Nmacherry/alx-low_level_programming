bf715351b71bc39c250340a5005f1eae1c869df9
Nmacherry <obasichidinma85@gmail.com>
2022-12-09 17:53:30 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints the alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while
+		(alpha <= 'z');
+	{
+		putchar(alpha);
+	alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
