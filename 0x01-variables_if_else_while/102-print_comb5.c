e28b479c5c53274ab830bc87c03f6e7075726ec4
Nmacherry <obasichidinma85@gmail.com>
2022-12-17 20:34:35 -0800

    task final

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,9 +9,39 @@
 
 int main(void)
 {
-	int p, q;
+	int c, i, k, j;
 
-	for (p = 0; p <= 98; p++)
+	for (c = 48; c <= 57; c++)
 	{
-	
-	for (q = p + 1; q <= 99; q++)
+		for (i = 48; i <= 57; i++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				for (j = 48; j <= 57; j++)
+				{
+					if (((k + j) > (c + i) &&  k >= c) || c < k)
+					{
+						putchar(c);
+						putchar(i);
+						putchar(' ');
+						putchar(k);
+						putchar(j);
+
+					if (c + i + k + j == 227 && c == 57)
+					{
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
