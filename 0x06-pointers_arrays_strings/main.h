7790ecd434556bb95e44a3ea6890be7fb5c5f0f9
Nmacherry <obasichidinma85@gmail.com>
2022-12-22 20:47:18 -0800

    magic

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,7 +8,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *str);
-int main(void);
 char *rot13(char *str);
 void print_number(int n);
 int _putchar(char c);
